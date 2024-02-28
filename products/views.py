824aec92dc376a7018f935f51579b2971c9566cf
DariaDrewa <daria.drewa97@gmail.com>
2024-02-28 19:03:46 +0100

    views

--- products/views.py
+++ products/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Products
 
 
 def products(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    myproducts = Products.objects.all().values()
+    template = loader.get_template("all_products.html")
+    context = {
+        myproducts: myproducts,
+    }
+    return HttpResponse(template.render(context, request))
+
+
