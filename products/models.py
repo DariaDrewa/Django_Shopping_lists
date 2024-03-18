bbb9e31cdf2b1341a041e6d20c370157065a0167
DariaDrewa <daria.drewa97@gmail.com>
2024-03-18 20:50:33 +0100

    add class Meta

--- products/models.py
+++ products/models.py
@@ -9,10 +9,15 @@ class Products(models.Model):
     def __str__(self):
         return f"{self.id}, {self.product_name}"
 
+    class Meta:
+        verbose_name_plural = "Produkty"
 
 class ShoppingLists(models.Model):
     lists_name = models.CharField(max_length=30)
-    products = models.ManyToManyField(Products, related_name="shoppinglists")
+    products = models.ManyToManyField(Products)
 
     def __str__(self):
         return f"{self.lists_name}"
+
+    class Meta:
+        verbose_name_plural = "Listy zakupów"
