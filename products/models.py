77eb6648ac601af61b25c5d567ac80502e749a82
DariaDrewa <daria.drewa97@gmail.com>
2024-05-25 12:37:53 +0200

    unique names of obj

--- products/models.py
+++ products/models.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 
 
 class Products(models.Model):
-    product_name = models.CharField(max_length=30, verbose_name="Nazwa produktu")
+    product_name = models.CharField(max_length=30, verbose_name="Nazwa produktu", unique=True,
+                                    error_messages={'unique': 'Taki produkt już istnieje!'})
 
     def __str__(self):
         return f"{self.product_name}"
@@ -13,7 +14,8 @@ class Products(models.Model):
 
 
 class ShoppingLists(models.Model):
-    lists_name = models.CharField(max_length=30, verbose_name="Nazwa listy")
+    lists_name = models.CharField(max_length=30, verbose_name="Nazwa listy", unique=True,
+                                  error_messages={'unique': 'Lista o takiej nazwie już istnieje!'})
     products = models.ManyToManyField(Products, verbose_name="Produkty")
 
     def __str__(self):
